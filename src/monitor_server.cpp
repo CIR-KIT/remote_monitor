e6dcadde7ec360df2d1ee889fa5a5016c86c740e
Arita Yuta <k104009y@mail.kyutech.jp>
2015-10-24 21:41:38 +0900

    [modify] third_robot_monitor_serverをクラス化

--- src/monitor_server.cpp
+++ src/monitor_server.cpp
@@ -3,19 +3,41 @@
 #include <tf/transform_broadcaster.h>
 #include "third_robot_monitor/TeleportAbsolute.h"
 
-bool getPos(third_robot_monitor::TeleportAbsolute::Request  &req,
-        third_robot_monitor::TeleportAbsolute::Response &res)
+class ThirdRobotMonitorServer
 {
-  std::cout<<"pos: "<<req.x<<" "<<req.y<<" "<<req.theta<<std::endl;
-  return true;
+public:
+	bool getPos(third_robot_monitor::TeleportAbsolute::Request  &req,
+				third_robot_monitor::TeleportAbsolute::Response &res);
+public:
+	ThirdRobotMonitorServer()
+	{
+		ros::NodeHandle n("~");
+
+		monitor_pub_ = nh_.advertise<nav_msgs::Odometry>("/monitor_odom", 1);
+		server_ = nh_.advertiseService<third_robot_monitor::TeleportAbsolute::Request,
+									   third_robot_monitor::TeleportAbsolute::Response>
+			("third_robot_monitor", boost::bind(&ThirdRobotMonitorServer::getPos, this, _1, _2));
+	}
+	
+	
+private:
+	ros::NodeHandle nh_;
+	ros::Publisher monitor_pub_;
+	ros::ServiceServer server_;
+};
+
+
+bool ThirdRobotMonitorServer::getPos(third_robot_monitor::TeleportAbsolute::Request  &req,
+			third_robot_monitor::TeleportAbsolute::Response &res)
+{	
+	std::cout<<"pos: "<<req.x<<" "<<req.y<<" "<<req.theta<<std::endl;
+	return true;
 }
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "third_robot_monitor_server");
-  ros::NodeHandle n;
-
-  ros::ServiceServer server = n.advertiseService("third_robot_monitor", getPos);
-  ROS_INFO("Ready to serve getPos.");
+  ThirdRobotMonitorServer monitor_server;
   ros::spin();
 
   return 0;

83fd20d0167c59d75fcdf4b963c5c3591ce4b9de
Arita Yuta <k104009y@mail.kyutech.jp>
2015-10-26 23:39:01 +0900

    [fix] 無駄にpublishしないように

--- src/monitor_server.cpp
+++ src/monitor_server.cpp
@@ -13,9 +13,6 @@ public:
 	{
 		ros::NodeHandle n("~");
 
-		odom_.header.frame_id = "monitor_odom";
-
-		monitor_pub_ = nh_.advertise<nav_msgs::Odometry>("/monitor_odom", 1);
 		server_ = nh_.advertiseService<third_robot_monitor::TeleportAbsolute::Request,
 									   third_robot_monitor::TeleportAbsolute::Response>
 			("third_robot_monitor", boost::bind(&ThirdRobotMonitorServer::getPos, this, _1, _2));
@@ -24,37 +21,15 @@ public:
 	
 private:
 	ros::NodeHandle nh_;
-	ros::Publisher monitor_pub_;
 	ros::ServiceServer server_;
-	nav_msgs::Odometry odom_;
-	tf::TransformBroadcaster monitor_odom_broadcaster_;
 };
 
 
 bool ThirdRobotMonitorServer::getPos(third_robot_monitor::TeleportAbsolute::Request  &req,
 			third_robot_monitor::TeleportAbsolute::Response &res)
 {
-	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(req.theta);
-	geometry_msgs::TransformStamped odom_trans;
-	odom_trans.header.stamp = ros::Time::now();
-	odom_trans.header.frame_id = "monitor_odom";
-	odom_trans.child_frame_id = "monitor_base_link";
-	odom_trans.transform.translation.x = req.x;
-	odom_trans.transform.translation.y = req.y;
-	odom_trans.transform.translation.z = 0.0;
-	odom_trans.transform.rotation = odom_quat;
-	
-	monitor_odom_broadcaster_.sendTransform(odom_trans);
-	  
-	odom_.header.stamp = ros::Time::now();
-	odom_.pose.pose.position.x = req.x;
-	odom_.pose.pose.position.y = req.y;
-	odom_.pose.pose.position.z = 0.0;
-	odom_.pose.pose.orientation = odom_quat;
-	monitor_pub_.publish(odom_);
-	
-	std::cout<<"pos: "<<req.x<<" "<<req.y<<" "<<req.theta<<std::endl;
-	return true;
+  ROS_INFO("Pos: [x] -> %6.2f, [y] -> %6.2f, [theta] -> %6.2f", req.x, req.y, req.theta);
+  return true;
 }
 
 int main(int argc, char **argv)

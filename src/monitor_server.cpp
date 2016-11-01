b9226da8040ca51a8d899601899ad64b1ea0753e
MoriKen254 <masamorobo@gmail.com>
2016-11-01 21:36:34 +0900

    [modify] ロボット位置を矢印先端の形状に変更．

--- src/monitor_server.cpp
+++ src/monitor_server.cpp
@@ -16,7 +16,7 @@ const static std::string MAP_WINDOW_NAME = "Map Monitor";
 const static int ROS_SPIN_RATE = 100;
 const static int CV_WAIT_KEY_RATE = 50;
 const static cv::Scalar RED = cv::Scalar(0, 0, 255);
-const static cv::Scalar GREEN = cv::Scalar(0, 255, 0);
+const static cv::Scalar GREEN = cv::Scalar(0, 128, 0);
 const static cv::Scalar BLUE = cv::Scalar(255, 0, 0);
 const static int ARROW_LENGTH = 10;
 
@@ -72,23 +72,67 @@ public:
 
     void drawRobotPoseOnMap(third_robot_monitor::TeleportAbsolute::Request &req)
     {
+        const cv::Point3f robot_pos(req.x, req.y, req.theta);
+        // only current point
+        drawArrow(map_img_pos_curr_, robot_pos);
+        // all pos history
+        drawArrow(map_img_pos_hist_, robot_pos);
+    }
+
+    void drawArrow(cv::Mat &img, const cv::Point3f pos)
+    {
         // only current pos
         map_img_ori_small_.copyTo(map_img_pos_curr_);
         //-- center of the robot on the map
-        double x_map_center = (req.x - map_origin_[INDEX_X]) * resize_ratio_curr_ / map_resolution_;
-        double y_map_center = map_img_pos_curr_.rows - (req.y - map_origin_[INDEX_Y]) / map_resolution_ * resize_ratio_curr_;
-        point_curr_ = cv::Point(x_map_center, y_map_center);
-        //-- arrow tip that represents the robot orientation on the map
-        double x_map_tip = x_map_center + ARROW_LENGTH * std::cos(req.theta);
-        double y_map_tip = y_map_center - ARROW_LENGTH * std::sin(req.theta);
-        point_tip_ = cv::Point(x_map_tip, y_map_tip);
-        //-- draw
-        cv::circle(map_img_pos_curr_, point_curr_, 2, RED, 3);
-        cv::line(map_img_pos_curr_, point_curr_, point_tip_, RED, 2);
+        double x_map_center = (pos.x - map_origin_[INDEX_X]) * resize_ratio_curr_ / map_resolution_;
+        double y_map_center = map_img_pos_curr_.rows - (pos.y - map_origin_[INDEX_Y]) / map_resolution_ * resize_ratio_curr_;
 
-        // all pos history
-        cv::circle(map_img_pos_hist_, point_curr_, 2, RED, 3);
-        cv::line(map_img_pos_hist_, point_curr_, point_tip_, RED, 2);
+        int w = 10;
+        int h = 15;
+        int lineType = 8;
+
+        /** Create some points */
+        cv::Point rook_points[1][4];
+        double p1_x_ori = (x_map_center-w*0.5) + w*0.5;
+        double p1_y_ori = (y_map_center-h*0.5) + 0.0;
+        double p2_x_ori = (x_map_center-w*0.5) + 0;
+        double p2_y_ori = (y_map_center-h*0.5) + h;
+        double p3_x_ori = (x_map_center-w*0.5) + w*0.5;
+        double p3_y_ori = (y_map_center-h*0.5) + h*0.7;
+        double p4_x_ori = (x_map_center-w*0.5) + w;
+        double p4_y_ori = (y_map_center-h*0.5) + h;
+
+        double theta = pos.z;
+        double cos_rot = cos(-theta + M_PI*0.5);
+        double sin_rot = sin(-theta + M_PI*0.5);
+
+        double p1_x_rot = cos_rot*(p1_x_ori-x_map_center) - sin_rot*(p1_y_ori-y_map_center);
+        double p1_y_rot = sin_rot*(p1_x_ori-x_map_center) + cos_rot*(p1_y_ori-y_map_center);
+        double p2_x_rot = cos_rot*(p2_x_ori-x_map_center) - sin_rot*(p2_y_ori-y_map_center);
+        double p2_y_rot = sin_rot*(p2_x_ori-x_map_center) + cos_rot*(p2_y_ori-y_map_center);
+        double p3_x_rot = cos_rot*(p3_x_ori-x_map_center) - sin_rot*(p3_y_ori-y_map_center);
+        double p3_y_rot = sin_rot*(p3_x_ori-x_map_center) + cos_rot*(p3_y_ori-y_map_center);
+        double p4_x_rot = cos_rot*(p4_x_ori-x_map_center) - sin_rot*(p4_y_ori-y_map_center);
+        double p4_y_rot = sin_rot*(p4_x_ori-x_map_center) + cos_rot*(p4_y_ori-y_map_center);
+
+        p1_x_rot += x_map_center;
+        p1_y_rot += y_map_center;
+        p2_x_rot += x_map_center;
+        p2_y_rot += y_map_center;
+        p3_x_rot += x_map_center;
+        p3_y_rot += y_map_center;
+        p4_x_rot += x_map_center;
+        p4_y_rot += y_map_center;
+
+        rook_points[0][0] = cv::Point(p1_x_rot, p1_y_rot);
+        rook_points[0][1] = cv::Point(p2_x_rot, p2_y_rot);
+        rook_points[0][2] = cv::Point(p3_x_rot, p3_y_rot);
+        rook_points[0][3] = cv::Point(p4_x_rot, p4_y_rot);
+
+        const cv::Point* ppt[1] = { rook_points[0] };
+        int npt[] = { 4 };
+
+        cv::fillPoly( img, ppt, npt, 1, GREEN, lineType );
     }
 
     void drawHumanPoseOnMap(third_robot_monitor::TeleportAbsolute::Request &req)
@@ -308,7 +352,35 @@ bool ThirdRobotMonitorServer::getHumanPose(third_robot_monitor::TeleportAbsolute
 
 int fillPolygonAndShow()
 {
-    int w = 800;
+    int w = 20;
+    cv::Mat img = cv::Mat::zeros( w, w, CV_8UC3 );
+    int lineType = 8;
+
+    /** Create some points */
+    cv::Point rook_points[1][4];
+    rook_points[0][0] = cv::Point(w*0.5, 0.0);
+    rook_points[0][1] = cv::Point(0, w);
+    rook_points[0][2] = cv::Point(w*0.5, w*0.7);
+    rook_points[0][3] = cv::Point(w, w);
+
+    const cv::Point* ppt[1] = { rook_points[0] };
+    int npt[] = { 4 };
+
+    cv::fillPoly( img, ppt, npt, 1, GREEN, lineType );
+
+    cv::Point2f center(img.cols*0.5, img.rows*0.5);
+    //const cv::Mat affine_matrix = cv::getRotationMatrix2D( center, angle, scale );
+
+    cv::imshow("test", img);
+    int key = cv::waitKey();
+
+    if(key == 27 || key == 'q' || key == 'Q')
+        return -1;
+}
+
+int humanDrawAndShow()
+{
+    int w = 20;
     cv::Mat img = cv::Mat::zeros( w, w, CV_8UC3 );
     int lineType = 8;
 
